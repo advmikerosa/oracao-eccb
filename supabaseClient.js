586ff7c790587b959ff5778d544bccbac00bfe09
Mike Rosa <advmikerosa@outlook.com>
2025-10-27 12:43:45 -0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,10 @@
+// src/supabaseClient.js
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://illgbfpmtcxiszihuyfh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsbGdiZnBtdGN4aXN6aWh1eWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NzM4MTUsImV4cCI6MjA3NzE0OTgxNX0.lKoU_mX_5q7dWEFi3wi7-eRC-rhmfe4tuIkJTbbSHhM"
+);
+
+export default supabase;
