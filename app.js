d393d581774aa65aa27cdf0b336563640fa97f3d
Mike Rosa <advmikerosa@outlook.com>
2025-10-27 14:06:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const supabaseUrl = 'https://illgbfpmtcxiszihuyfh.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsbGdiZnBtdGN4aXN6aWh1eWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NzM4MTUsImV4cCI6MjA3NzE0OTgxNX0.lKoU_mX_5q7dWEFi3wi7-eRC-rhmfe4tuIkJTbbSHhM'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://illgbfpmtcxiszihuyfh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Data Management
 async function registrarOracao() {
